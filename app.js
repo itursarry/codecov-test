9042d21fdec7546290a5c325c8211054b58129f0
Rodrigo Itursarry <rodrigo.itursarry@rixsus.com>
2023-03-06 01:29:30 -0300

    Test4

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+
+var router = express.Router();
 const port = 3000
 
 var indexRouter = require('./src/routes/index');
 
-app.use('/', indexRouter);
+
+app.use('/', router.get('/', indexRouter));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
